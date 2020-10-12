2c26ea0dd235ea6d2c8f8d2885387d98d82762ee
Ady Buxton <ady.buxton@fujitsu.com>
2020-10-12 12:29:38 +0100

    Simplified first dashboard

--- dashboards-jsonnet/convert.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-SCRIPT_PATH="/dashboards-jsonnet"
-for file in $SCRIPT_PATH/*.jsonnet
-do 
-    input=$(basename -- $file)
-    output=$SCRIPT_PATH/${input%.*}.json
-    echo "Converting $file into $output"
-    jsonnet $file > $output
-done
